2414f9f4440af6647e28039f7959a65202601ce9
spt4m1m <white.hat.tamim@gmail.com>
2023-05-18 22:39:48 +0600

    added about section in homepage

---
+++ src/Pages/HomePage/AboutUs.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import aboutimg from '../../assets/childrenplayingwithtoy.jpeg'
+
+const AboutUs = () => {
+    return (
+        <div>
+            <div className="hero min-h-[70vh] py-10">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src={aboutimg} className="max-w-sm md:max-w-2xl rounded-lg shadow-2xl" />
+                    <div className='md:ms-10'>
+                        <h1 className="text-5xl font-bold">About Us</h1>
+                        <p className="py-6">
+                            Introducing Animals ToyHub, the ultimate online destination for all your toy-buying needs! As a leading toy-selling website, we offer an extensive collection of toys that will ignite the imagination and bring joy to children of all ages.
+
+                            At Animals ToyHub, we understand the importance of play in a child's development. That's why we carefully curate our product selection to encompass a wide range of toys that cater to various interests, learning styles, and age groups. From classic toys that have stood the test of time to the latest trends and innovations, we have something to suit every child's preferences.</p>
+                        <button className="btn btn-primary normal-case">know more</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Support from './Support';
+import AboutUs from './AboutUs';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Support />
+            <AboutUs />
         </div>
     );
 };
