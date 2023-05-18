6fa56435b6a139761fc39f09fa05aaa5e3066e7a
spt4m1m <white.hat.tamim@gmail.com>
2023-05-18 12:52:57 +0600

    mytoy page implement user toy added displayed data

--- src/Pages/MyToysPage/MyToys.jsx
+++ src/Pages/MyToysPage/MyToys.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Providers/AuthProvider';
+import MyToy from './MyToy';
 
 const MyToys = () => {
+    const [toys, setToys] = useState([])
+    const { user } = useContext(AuthContext)
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/alltoys?email=${user.email}`)
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [])
+
     return (
         <div>
-            <h1 className='text-center text-3xl underline'>My Toy</h1>
+            <h1 className='text-center text-3xl'>{`You added ${toys.length} Toys`}</h1>
+
+            <div className='grid grid-cols-1 md:grid-cols-3'>
+                {
+                    toys.map(toy => <MyToy toy={toy} key={toy._id} />)
+                }
+            </div>
+
         </div>
     );
 };
