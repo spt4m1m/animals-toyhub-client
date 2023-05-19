939c98771161fea62e61760a202f73acb4bc3268
spt4m1m <white.hat.tamim@gmail.com>
2023-05-20 03:22:55 +0600

    updated register page design

--- src/Pages/RegisterPage/Register.jsx
+++ src/Pages/RegisterPage/Register.jsx
@@ -50,6 +50,7 @@ const Register = () => {
                 const user = result.user;
                 toast.success(`Successfull login with ${user.displayName}`)
                 setLoading(false)
+                navigate('/')
             })
             .catch(error => {
                 if (error.message == 'Firebase: Error (auth/popup-closed-by-user).') {
@@ -62,108 +63,92 @@ const Register = () => {
 
     return (
         <HelmetProvider>
-            <section className="h-screen">
-                <Helmet>
-                    <title>Animals Toyhub | Register</title>
-                </Helmet>
-                <div className="px-6 h-full">
-                    <div
-                        className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
-                    >
-                        <div
-                            className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
-                        >
-                            <img
-                                src={registerImg}
-                                className="w-full"
-                                alt=""
-                            />
-                        </div>
-                        <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-                            <h1 className='text-2xl text-center font-semibold py-10 underline-offset-2'>Please Register !</h1>
-                            <form onSubmit={handleRegisterAUser}>
-                                {/* <!-- Name input --> */}
-                                <div className="mb-6">
-                                    <input
-                                        type="text"
-                                        name='name'
-                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary focus:outline-none"
-                                        placeholder="Your Name" required
-                                    />
+            <Helmet>
+                <title>Animals Toyhub | Register</title>
+            </Helmet>
+            <div>
+                <div className="min-w-screen min-h-screen flex items-center justify-center px-5 py-5">
+                    <div className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden max-w-[1000px]" >
+                        <div className="md:flex w-full">
+                            <div className="hidden md:block w-1/2 bg-indigo-500 py-10 px-10">
+                                <img src={registerImg} alt="" />
+                            </div>
+                            <div className="w-full md:w-1/2 py-10 px-5 md:px-10">
+                                <div className="text-center mb-10">
+                                    <h1 className="font-bold text-3xl text-gray-900">REGISTER</h1>
+                                    <p>Enter your information to register</p>
                                 </div>
-                                {/* <!-- Photo Link input --> */}
-                                <div className="mb-6">
-                                    <input
-                                        type="text"
-                                        name='photoUrl'
-                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary focus:outline-none"
-                                        placeholder="Photo Url"
-                                    />
-                                </div>
+                                <form onSubmit={handleRegisterAUser}>
+                                    <div>
+                                        <div className="flex -mx-3">
+                                            {/* name  */}
+                                            <div className="w-1/2 px-3 mb-5">
+                                                <label className="text-xs font-semibold px-1">Name</label>
+                                                <div className="flex">
+                                                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
+                                                    <input name='name' type="text" className="w-full -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="name" required />
+                                                </div>
+                                            </div>
+                                            {/* photo url  */}
+                                            <div className="w-1/2 px-3 mb-5">
+                                                <label className="text-xs font-semibold px-1">Photo URL</label>
+                                                <div className="flex">
+                                                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
+                                                    <input name='photoUrl' type="text" className="w-full -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="photo url" required />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* email  */}
+                                        <div className="flex -mx-3">
+                                            <div className="w-full px-3 mb-5">
+                                                <label className="text-xs font-semibold px-1">Email</label>
+                                                <div className="flex">
+                                                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
+                                                    <input name='email' type="email" className="w-full -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="example@gmail.com" required />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* pass  */}
+                                        <div className="flex -mx-3">
+                                            <div className="w-full px-3 mb-12">
+                                                <label className="text-xs font-semibold px-1">Password</label>
+                                                <div className="flex">
+                                                    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i className="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
+                                                    <input name='password' type="password" className="w-full -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="************" required />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* or  */}
+                                        <div
+                                            className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
+                                        >
+                                            <p className="text-center font-semibold mx-4 mb-0 text-black">Or</p>
+                                        </div>
 
-                                {/* <!-- Email input --> */}
-                                <div className="mb-6">
-                                    <input
-                                        type="email"
-                                        name='email'
-                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary focus:outline-none"
-                                        placeholder="Email address" required
-                                    />
-                                </div>
-
-                                {/* <!-- Password input --> */}
-                                <div className="mb-6">
-                                    <input
-                                        type="password"
-                                        name='password'
-                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-primary focus:outline-none"
-                                        placeholder="Password" required
-                                    />
-                                </div>
-
-                                {/* pass end */}
-                                <div
-                                    className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
-                                >
-                                    <p className="text-center font-semibold mx-4 mb-0 text-black">Or</p>
-                                </div>
-
-                                {/* or  */}
-                                <div className="flex flex-row items-center justify-center lg:justify-start">
-                                    <p className="text-xl mb-0 mr-4 text-black">Register with -:</p>
-                                    <button
-                                        onClick={handleGoogleLogin}
-                                        type="button"
-                                        data-mdb-ripple="true"
-                                        data-mdb-ripple-color="light"
-                                        className='text-4xl'
-                                    ><Icon icon="flat-color-icons:google" />
-                                    </button>
-                                </div>
-
-
-                                {/* register button */}
-
-                                <div className="text-center lg:text-left">
-                                    <button
-                                        type="submit"
-                                        className="inline-block px-7 py-3 bg-primary text-white font-medium text-xl leading-snug rounded shadow-md hover:bg-white hover:text-primary hover:shadow-lg focus:bg-white focus:text-primary focus:shadow-lg focus:outline-none focus:ring-0 active:bg-white active:shadow-lg transition duration-150 ease-in-out border-2 border-primary w-full my-5"
-                                    >
-                                        Register
-                                    </button>
-                                    <p className="text-sm font-semibold mt-2 pt-1 mb-0 text-black">
-                                        Already Have an account?
-                                        <Link
-                                            to="/login"
-                                            className="text-primary underline transition duration-200 ease-in-out"
-                                        > Login </Link>
-                                    </p>
-                                </div>
-                            </form>
+                                        <div className="flex flex-row items-center justify-center lg:justify-start">
+                                            <p className="text-xl mb-5 mr-4 text-black font-semibold">REGISTER WITH -:</p>
+                                            <span
+                                                onClick={handleGoogleLogin}
+                                                className='text-4xl mb-5 cursor-pointer'
+                                            ><Icon icon="flat-color-icons:google" />
+                                            </span>
+                                        </div>
+                                        <div className="flex -mx-3">
+                                            <div className="w-full px-3 mb-5">
+                                                <button
+                                                    type="submit"
+                                                    data-mdb-ripple="true"
+                                                    data-mdb-ripple-color="light"
+                                                    className="block w-full max-w-xs mx-auto btn btn-primary normal-case">REGISTER NOW</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
         </HelmetProvider>
     );
 };
